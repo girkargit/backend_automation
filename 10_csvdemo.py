16b555cc2e718d7f11413b02bc3b7a0d3cba30ef
girkargit <abhilashgirkar97@gmail.com>
2024-04-20 17:26:48 +0530

    Change

--- 10_csvdemo.py
+++ 10_csvdemo.py
@@ -1,6 +1,20 @@
 import csv
 
+name_lst = []
+status_lst = []
 with open("utilities/loanapp.csv") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    print(csvreader)
-    print(list(csvreader))
+    # print(csvreader)
+    # print(list(csvreader))
+    for row in csvreader:
+        name_lst.append(row[0])
+        status_lst.append(row[1])
+print(name_lst)
+print(status_lst)
+
+# index = name_lst.index("Satish")
+print("Loan status is %s." % status_lst[name_lst.index("Satish")])
+
+with open("utilities/loanapp.csv", "a") as wfile:
+    write = csv.writer(wfile)
+    write.writerow(["Suresh" , "approved"])
